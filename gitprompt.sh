b9e70e506d8f19570f11e12b9e7f6e2fde53b9cb
W. Evan Sheehan <52247+darthmall@users.noreply.github.com>
2017-01-30 13:46:35 -0500

    Generate a smaller path string to save space in the prompt

--- gitprompt.sh
+++ gitprompt.sh
@@ -1,36 +1,53 @@
-function git_summary() {
-  if [[ -e $1 ]]; then
-    return
-  fi
-
-  echo $_branch
-}
-
 function prompt {
+  # Load first line of git status for branch info
   local st=$(git status --porcelain -b 2>/dev/null | head -n 1)
 
+  # An empty string means we're not in a git repo and can skip this part
   if [[ -n $st ]]; then
     local branchname=$(echo $st | sed -nE "s/^## ([^.]+).*/\1/p")
 
+    # Add count of commits we are ahead
     local ahead=$(echo $st | sed -nE "s/^##.*ahead ([0-9]+).*/\1/p")
     if [[ -n $ahead ]]; then
       ahead=" \[\e[0;31m\]▲$ahead"
     fi
 
+    # Add count of commits we are behind
     local behind=$(echo $st | sed -nE "s/^##.*behind ([0-9]+).*/\1/p")
     if [[ -n $behind ]]; then
       behind=" ▼$behind"
     fi
 
-    declare -i delta=$(git status --porcelain -b | wc -l)
+    # Count number of changed/unstaged files
+    local delta=$(git status --porcelain -b | wc -l)
     if [[ $delta -gt 1 ]]; then
       local dirty=$(printf " \[\e[0;31m\]𝚫%d" $(($delta - 1)))
     fi
 
+    # Forat the git message
     local git_msg="[\[\e[0;32m\]$branchname$behind$ahead$dirty\[\e[m\]] "
   fi
 
-  export PS1=$(echo -e "\w $git_msg\xC2\xA7 ")
+  # Build a string for the CWD by replacing $HOME with ~
+  local CWD=${PWD/$HOME/'~'}
+  
+  # Tokenize path
+  IFS="/" read -a NODES <<< $(sed -E "s/^\/?(.*)\/?$/\1/" <<< $CWD)
+  
+  # Truncate long paths by inserting an ellipsis instead of enumerating
+  # full path
+  if [[ ${#NODES[@]} -gt 3 ]]; then
+    local IDX=$(expr ${#NODES[@]} - 1)
+    local CWD=${NODES[0]}"/…/"${NODES[$IDX]}
+    
+    if [[ ${NODES[0]} != "~" ]]; then
+      local CWD="/"$CWD
+    fi
+  fi
+  
+  # Set prompt, using echo to ensure that unicode character at the end is
+  # printed correctly in the terminal
+  export PS1=$(echo -e "$CWD $git_msg\xC2\xA7 ")
 }
 
 export PROMPT_COMMAND=prompt
