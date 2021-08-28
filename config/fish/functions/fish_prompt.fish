148548dfac60f5f4a5da0c72340dec1cb6fcd0d6
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-08-28 13:03:00 -0600

    Fix the prompt
    I had an error in my echo syntax that resulted in no path being
    displayed unless you were in a Git repo. I replaced the echo with a
    printf, although I'm not sure that compressing the prompt to a single
    line is an improvement over the echo statements, which were less terse
    and didn't include an unintelligible format string.
    
    For the record, you have to separate each argument to echo with a space,
    so to avoid spaces between the path and the Git prompt, you need `echo
    -s`, which removes spaces between the arguments.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,8 +2,7 @@ function fish_prompt
   set -g __fish_git_prompt_use_informative_chars
   set -g __fish_git_prompt_showdirtystate
   set -g __fish_git_prompt_showcolorhints
-  echo
-  echo (prompt_pwd)(fish_git_prompt)
-  echo '> '
+
+  printf '\n%s%s\n> ' (prompt_pwd) (fish_git_prompt)
 end
 
