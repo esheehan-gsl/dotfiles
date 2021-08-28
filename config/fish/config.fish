0f821a9749764313540c6e639812b6d708f17e83
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-08-28 12:53:24 -0600

    Add some abbreviations for fish
    Add some of the abbreviations I like for fish. A couple that replicate
    commands available on macOS, and a few shortcuts for some of the CLIs I
    like

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+  abbr --add --global pbcopy 'xsel --clipboard --input'
+  abbr --add --global pbpaste 'xsel --clipboard --output'
+  abbr --add --global open 'xdg-open &>/dev/null'
+
+  abbr --add --global rm 'rm -I'
+
+  abbr --add --global l 'ls -lh'
+  abbr --add --global ll 'ls -lha'
+
+  abbr --add --global fd 'fdfind'
+  abbr --add --global find 'fdfind'
+end
