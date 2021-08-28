29ae4415943afa1cfa74d00c558563b323478f0e
W. Evan Sheehan <evan.sheehan@noaa.gov>
2021-08-27 21:25:50 -0600

    Remove some extraneous white space from the prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
   set -g __fish_git_prompt_showdirtystate
   set -g __fish_git_prompt_showcolorhints
   echo
-  echo (prompt_pwd) (fish_git_prompt)
+  echo (prompt_pwd)(fish_git_prompt)
   echo '> '
 end
 
