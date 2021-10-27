c5e553fc8b795d56abcf87c772fec31fd076817e
W. Evan Sheehan <52247+darthmall@users.noreply.github.com>
2021-10-27 13:37:10 -0600

    Add fish configuration

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+  abbr --add --global pbcopy 'xsel --clipboard --input'
+  abbr --add --global pbpaste 'xsel --clipboard --output'
+  abbr --add --global open 'xdg-open &>/dev/null'
+
+  abbr --add --global rm 'rm -I'
+
+  abbr --add --global l 'ls -lh'
+  abbr --add --global ll 'ls -lh --all'
+
+  abbr --add --global tree 'exa --tree'
+
+  abbr --add --global find 'fdfind'
+
+  abbr --add --global ps 'procs'
+end
