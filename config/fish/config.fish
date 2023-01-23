fde568303d4a48b6b978de59679fce4eb96c401a
W. Evan Sheehan <evan.sheehan@noaa.gov>
2023-01-23 09:34:33 -0700

    Fix whitespace in fish config
    Convert the fish config to tabs instead of spaces. Also added a modeline
    to the fish config.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,34 +1,38 @@
+# Author: Evan Sheehan <darth_mall@pm.me>
+# Description: fish configuration
+# vim: set fdm=marker fdl=0 ts=2 sw=2 tw=80 :
+
 if status is-login
-  pyenv init --path | source
+	pyenv init --path | source
 end
 
 if status is-interactive
-  pyenv init - | source
-  
-  fish_add_path ~/.local/bin
+	pyenv init - | source
 
-  set -gx EDITOR vim
-  set --universal nvm_default_version lts
+	fish_add_path ~/.local/bin
 
-  abbr --add --global pbcopy 'xclip -in -selection clipboard'
-  abbr --add --global pbpaste 'xclip -out -selection clipboard'
-  abbr --add --global open 'xdg-open &>/dev/null'
+	set -gx EDITOR vim
+	set --universal nvm_default_version lts
 
-  abbr --add --global rm 'rm -I'
+	abbr --add --global pbcopy 'xclip -in -selection clipboard'
+	abbr --add --global pbpaste 'xclip -out -selection clipboard'
+	abbr --add --global open 'xdg-open &>/dev/null'
 
-  abbr --add --global l 'exa --long --header --git'
-  abbr --add --global ll 'exa --long --header --git --all'
-  abbr --add --global lt 'exa --long --header --git --all --tree'
-  abbr --add --global tree 'exa --tree'
+	abbr --add --global rm 'rm -I'
 
-  abbr --add --global fd 'fdfind'
-  abbr --add --global 'dci' 'docker rmi (docker images -a --filter=dangling=true -q)'
+	abbr --add --global l 'exa --long --header --git'
+	abbr --add --global ll 'exa --long --header --git --all'
+	abbr --add --global lt 'exa --long --header --git --all --tree'
+	abbr --add --global tree 'exa --tree'
 
-  abbr --add --global p 'procs'
+	abbr --add --global fd 'fdfind'
+	abbr --add --global 'dci' 'docker rmi (docker images -a --filter=dangling=true -q)'
 
-  abbr --add --global c 'batcat'
+	abbr --add --global p 'procs'
+
+	abbr --add --global c 'batcat'
 end
 
 if test -e ~/.config/fish/local.fish
-  source ~/.config/fish/local.fish
+	source ~/.config/fish/local.fish
 end
