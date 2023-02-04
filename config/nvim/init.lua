13fe5f51dc5673ca181a29998240488415767841
W. Evan Sheehan <darth_mall+github@pm.me>
2023-02-04 10:08:43 -0700

    Set install colorscheme

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,7 +18,9 @@ if not vim.loop.fs_stat(lazypath) then
 		})
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	colorsheme = "nord",
+})
 -- }}}
 
 require("autocmds");
